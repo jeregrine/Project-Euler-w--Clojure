e1aefbb91d2f295837ba2c28646ba08497a2477b
Jason <stiebs.81@gmail.com>
2009-10-20 20:01:43 -0500

    Solution provided via IRC need to study this closer

--- problem5.clj
+++ problem5.clj
@@ -1,19 +1,11 @@
 
-(defn finder
-	[n]
-	(loop [result false x 20]
-		(if (= x 10) 
-			true 
-			(if (= (rem x n) 0 ) 
-				(recur false (dec x))
-				false))))
-				
-		
-(defn findDevisible 
-	"What is the smallest number that is evenly divisible by all of the numbers from 1 to 20?"
-	[n]
-	(loop [result false x n]
-		(do (println x))
-		(if (finder x)
-				(str "Value is..." x)
-				(recur false (+ x 2520)))))
+
+(defn find-divisible [r]
+	(first 
+		(drop-while 
+			(fn [n] 
+				(not 
+					(every? 
+						#(zero? (rem n %)) r))) 
+							(iterate inc 1))))
+	
